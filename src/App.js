b0a5bbb73cd5ae71510cffda9e0afd227253244f
mfcbentes <mfcbentes.stm@gmail.com>
2021-04-19 22:28:29 -0300

    Updated on 'master'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import 'antd/dist/antd.css';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Layout className="layout">
+        <Header>
+          <div className="logo">
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">Home</Menu.Item>
+            </Menu>
+          </div>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <div className="site-layout-content">
+            <p>Conteudo</p>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>eVaccine - Fullstack Week</Footer>
+      </Layout>
     </div>
   );
 }
